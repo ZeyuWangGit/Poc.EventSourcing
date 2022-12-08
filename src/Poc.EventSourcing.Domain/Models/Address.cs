b8aac95429a281aba27b071b7541f5029406759e
zeyuwangFranklin <wzy.franklin@gmail.com>
2022-12-08 22:25:36 +0800

    Client Model

---
+++ src/Poc.EventSourcing.Domain/Models/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poc.EventSourcing.Domain.Models
+{
+    public class Address
+    {
+        public Address(string physicalAddress, string postCode, string state, string country)
+        {
+            PhysicalAddress = physicalAddress;
+            PostCode = postCode;
+            State = state;
+            Country = country;
+        }
+        public string PhysicalAddress { get; set; }
+        public string PostCode { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+
+    }
+}
---
+++ src/Poc.EventSourcing.Domain/Models/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetDevPack.Domain;
+
+namespace Poc.EventSourcing.Domain.Models
+{
+    public class Client : Entity, IAggregateRoot
+    {
+        public Client(Guid id, string? name, string? clAccountId, Address? address)
+        {
+            Id = id;
+            Name = name;
+            ClAccountId = clAccountId;
+            Address = address;
+        }
+
+        protected Client() {}
+
+        public string? Name { get; private set; }
+        public string? ClAccountId { get; private set; }
+        public Address? Address { get; private set; }
+    }
+}
