d7dcb5ff311ecd5ad2d1fc6f6ca94369ec1bc2f9
zeyuwangFranklin <wzy.franklin@gmail.com>
2022-12-11 22:10:29 +0800

    DBConetext

---
+++ src/Poc.EventSourcing.Infra.Data/Context/EFDbContext.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation.Results;
+using Microsoft.EntityFrameworkCore;
+using NetDevPack.Data;
+using NetDevPack.Mediator;
+using NetDevPack.Messaging;
+using Poc.EventSourcing.Domain.Models;
+using Poc.EventSourcing.Infra.Data.Mapping;
+
+namespace Poc.EventSourcing.Infra.Data.Context
+{
+    public sealed class EFDbContext : DbContext, IUnitOfWork
+    {
+        private readonly IMediatorHandler _mediatorHandler;
+        public EFDbContext(DbContextOptions<EFDbContext> options, IMediatorHandler mediatorHandler) : base(options)
+        {
+            _mediatorHandler = mediatorHandler;
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Ignore<ValidationResult>();
+            modelBuilder.Ignore<Event>();
+
+            modelBuilder.ApplyConfiguration(new ClientMapper());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public Task<bool> Commit()
+        {
+                throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Poc.EventSourcing.Infra.Data/Mapping/ClientMapper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Poc.EventSourcing.Domain.Models;
+
+namespace Poc.EventSourcing.Infra.Data.Mapping
+{
+    public class ClientMapper : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.Property(c => c.Id)
+                .HasColumnName("Id");
+
+            builder.Property(c => c.Name)
+                .HasColumnType("varchar(100)")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(c => c.ClAccountId)
+                .HasColumnType("varchar(20)")
+                .HasMaxLength(20)
+                .IsRequired();
+        }
+    }
+}
